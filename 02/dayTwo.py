35f472cc90bd43d935ffaa99d2087bd7a23b0ca3
Kilpatrick <nicholas.kilpatrick@gmail.com>
2022-12-08 16:16:47 -0500

    forgot I had to manually save the files before committing

--- 02/dayTwo.py
+++ 02/dayTwo.py
@@ -37,16 +37,21 @@ def convert(shape):
     elif shape == 'Z':
         return 'C'
 
+def solvePartOne():
+    for round in rps_rounds:
+        round_shapes = round.split()
+        opp = round_shapes[0]
+        you = convert(round_shapes[1])
+        round_score += points_for_shape(you)
+        round_score += points_for_result(opp, you)
+        print("round score: " + str(round_score))
+        total_score += round_score
+        round_score = 0
+
+    print(total_score)
+
+
+
 total_score = 0
 round_score = 0
-for round in rps_rounds:
-    round_shapes = round.split()
-    opp = round_shapes[0]
-    you = convert(round_shapes[1])
-    round_score += points_for_shape(you)
-    round_score += points_for_result(opp, you)
-    print("round score: " + str(round_score))
-    total_score += round_score
-    round_score = 0
-
-print(total_score)
+solvePartOne()
---
+++ 02/notes_day_2.md
@@ -0,0 +1,11 @@
+A = Rock, 
+B = Paper,
+C = Scissors
+
+X = Rock,
+Y = Paper,
+Z = Scissors
+
+Total score = sum of scores for each round
+
+Round score = score for shape played( rock= **1**, paper= **2**, scissors= **3**) + score of round result( loss= **0**, draw= **3**, win= **6**)
